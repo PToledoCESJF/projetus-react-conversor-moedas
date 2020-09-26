cd38d9c3ab79a4e6611d9a52772b6373bc7bf979
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-09-26 15:39:07 -0300

    refactor: criando pastas test e service, renomeando arquivos e importando o bootstrap

---
+++ src/test/conversor-moedas.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import ConversorMoedas from '../conversor-moedas';
+
+describe('Conversor de moedas', () => {
+
+  test('Deve renderizar o componente sem erros', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ConversorMoedas />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
