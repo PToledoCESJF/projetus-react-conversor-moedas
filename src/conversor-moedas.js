0366ec493e069a71d5a102f80a885ff6e35fa7cf
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-09-26 17:48:18 -0300

    feat: criando o componente de listagem de moedas

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -3,6 +3,7 @@ import './conversor-moedas.css';
 import { Jumbotron, Button, Form, Col, Spinner, Alert, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+import ListarMoedas from './listar-moedas';
 
 function ConversorMoedas() {
   return (
@@ -17,11 +18,13 @@ function ConversorMoedas() {
             <Col sm="3"><Form.Control placeholder="0" value={1} required /></Col>
             <Col sm="3">
               <Form.Control as="select">
+              <ListarMoedas />
               </Form.Control>
             </Col>
             <Col sm="1" className="text-center" style={{paddingTop: '5px'}}><FontAwesomeIcon icon={faAngleDoubleRight} /></Col>
             <Col sm="3">
               <Form.Control as="select">
+                <ListarMoedas />
               </Form.Control>
             </Col>
             <Col sm="2">
