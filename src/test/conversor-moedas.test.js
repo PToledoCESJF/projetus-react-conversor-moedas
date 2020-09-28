9a4eadb4b1af618c6340e67bf6b9833b23bdca17
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-09-28 19:39:40 -0300

    fix: o teste que simula uma conversao esta quebrando, verificar qual e o problema

--- src/test/conversor-moedas.test.js
+++ src/test/conversor-moedas.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ConversorMoedas from '../conversor-moedas';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+import axiosMock from 'axios';
 
 describe('Conversor de moedas', () => {
   test('Deve renderizar o componente sem erros', () => {
@@ -8,4 +11,21 @@ describe('Conversor de moedas', () => {
     ReactDOM.render(<ConversorMoedas />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+
+  /* BUG 
+  *   Esse teste não está funcionando,
+  *   Verificar qual é o problema
+  *   Erro: Incapaz de encontrar um elemento por: [data-testid="modal"]
+  *
+  test('Deve simular uma conversão de moedas', async () => {
+    const { findByTestId, getByTestId } = render(<ConversorMoedas />);
+    axiosMock.get.mockResolvedValueOnce({
+      data: {success: true, rates: { BRL: 4.564292, USD: 1.101049 }}
+    });
+    fireEvent.click(getByTestId('btn-converter'));
+    const modal = await findByTestId('modal');
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(modal).toHaveTextContent('1 BRL = 0.24 USD');
+  });
+  */
 });
